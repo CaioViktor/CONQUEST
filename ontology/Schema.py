c53bcebc51b867227d9c9c0a2b5923073c6cab03
CaioViktor <caioviktor@hotmail.com>
2019-07-30 17:37:32 -0300

    starting class index

--- ontology/Schema.py
+++ ontology/Schema.py
@@ -2,19 +2,34 @@ import os
 from rdflib import Graph
 from rdflib.util import guess_format
 import hashlib
-from rdflib import OWL, RDF, RDFS, OWL, XSD
+from rdflib.resource import Resource
+from rdflib import OWL, RDF, RDFS, OWL, XSD,URIRef,BNode
 from rdflib.namespace import Namespace
+
 DC = Namespace('http://purl.org/dc/elements/1.1/')
 DCTERMS = Namespace('http://purl.org/dc/terms/')
 
+
+
 #Load Graph
-def getGraph(path):
+def getGraph(file,path="configurations/"):
+	file_path = os.path.join(path,file)
+	
 	g = Graph()
-	# print(guess_format(path))
-	g.parse(path,format=guess_format(path))
-	# g.parse(path,format="turtle")
+	# print(guess_format(file_path))
+	g.parse(file_path,format=guess_format(file_path))
+	# g.parse(file_path,format="turtle")
 	return g
 
+
+def get_Resource(graph,uri):
+	if type(uri) is str:
+		uri = URIRef(uri)
+	return Resource(graph,uri)
+
+
+def uri_to_hash(uri):
+	return hashlib.md5(str(uri).encode('utf-8')).hexdigest()
 
 def get_range_damain(schema,types=RDF.Property):
 	properties = {}
@@ -35,15 +50,14 @@ def get_range_damain(schema,types=RDF.Property):
 		for label in schema.preferredLabel(prop):
 			labels.append(label[1])
 
-		id_hash = hashlib.md5(str(prop).encode('utf-8')).hexdigest()
+
+		id_hash = uri_to_hash(prop)
 		properties[id_hash] = {'uri':prop,'domains':domains,'ranges':ranges,'labels':labels,'comments':comments}
-		print("{}\n\tDomain:{}\n\tRange:{}\n\tLabels:{}\n\tComments:{}".format(prop,domains,ranges,labels,comments))
+		#print("{}\n\tDomain:{}\n\tRange:{}\n\tLabels:{}\n\tComments:{}".format(prop,domains,ranges,labels,comments))
 	return properties
 
 
-def load_properties_index(file,path="configurations/"):
-	file_path = os.path.join(path,file)
-	schema = getGraph(file_path)
+def load_properties_index(schema):
 
 	# print("Get properties")
 	#Get properties
@@ -57,3 +71,71 @@ def load_properties_index(file,path="configurations/"):
 	#Get DatatypeProperty
 	dtp = get_range_damain(schema,OWL.DatatypeProperty)
 	return p,op,dtp
+
+
+
+#Get all information for make the index of a class
+def get_info_class(classe,schema):
+	info = {'uri':classe}
+
+
+	#get labels
+	labels = []
+	for label in schema.preferredLabel(classe):
+		labels.append(label[1])
+	info['labels'] = labels
+
+
+	#get own properties
+	properties = []
+	for propertyO in schema.subjects(RDFS.domain,classe):
+		properties.append(propertyO)
+	info['properties'] = properties
+
+
+	#get incident properties
+	incident_properties = []
+	for propertyO in schema.subjects(RDFS.range,classe):
+		incident_properties.append(propertyO)
+	info['incident_properties'] = incident_properties
+
+
+	#get super-classes
+	super_classes_list = get_Resource(schema,classe).transitive_objects(RDFS.subClassOf)
+	super_classes = []
+	for super_class in super_classes_list:
+
+		if not isinstance(super_class.identifier,BNode) :
+			super_classes.append(super_class.identifier)
+
+
+
+
+	print("Classe:{}\n".format(classe))
+	print(super_classes)
+	print("\nTeste 2:\n")
+	r = get_Resource(schema,classe)
+	r = URIRef(r.identifier)
+	for test in schema.objects(r,RDFS.subClassOf):
+		print(test)
+	print("\n*********************\n")
+
+	
+
+
+	return info
+
+def load_classes_index(schema):
+	classes = {}
+	#Get OWL:Classes
+	for classe in schema.subjects(RDF.type,OWL.Class):
+		uri = uri_to_hash(classe)
+		classes[uri] = get_info_class(classe,schema)
+
+	#Get RDFS:Classes
+	for classe in schema.subjects(RDF.type,RDFS.Class):
+		uri = uri_to_hash(classe)
+		classes[uri] = get_info_class(classe,schema)
+
+
+	return classes
--- test.py
+++ test.py
@@ -1,28 +1,15 @@
 import pprint
 import qai.QAI_Manager as q
 import ontology.Schema as sc
-import ontology.SPARQL_Parser.sqparser3 as parser
 
 
 # QAIM = q.QAI_Manager("example_QAIs.js")
-#sc.load_properties_index("ontology.ttl")
-
-text = '''PREFIX foaf:<http://xmlns.com/foaf/0.1/> 
-		SELECT ?name ?idade WHERE{ 
-			?a a foaf:Person; 
-				foaf:name ?name; 
-				foaf:age ?age. 
-			FILTER(REGEX(STR(?name), $nome ,'i'))
-		}'''
-
-text = '''PREFIX qpr: <http://istresearch.com/qpr> 
- 			SELECT ?number_of_individuals (count(?ad) AS ?count)(group_concat(?ad;separator=',') AS ?ads) WHERE { 
- 				?ad a qpr:Ad ; 
- 				qpr:location 'Eugene, OR' ; 
- 				qpr:number_of_individuals ?number_of_individuals .  
- 			} GROUP BY ?number_of_individuals 
- 			ORDER BY DESC(?count) 
- 			LIMIT 1'''
 
 pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(parser.SQParser.parse_string(text))
+
+schema = sc.getGraph("ontology.ttl")
+# pp.pprint(sc.load_properties_index(schema))
+sc.load_classes_index(schema)
+# pp.pprint(sc.load_classes_index(schema))
+
+schema.close()
