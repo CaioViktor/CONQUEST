32e5f8d4a5d0d44e9a3fe383b9f5e4798dcb2487
CaioViktor <caioviktor@hotmail.com>
2019-09-16 16:36:41 -0300

    starting classifier

---
+++ classifier/ML_Classifier.py
@@ -0,0 +1,21 @@
+class ML_Classifier():
+	def __init__(self):
+		return
+	def __init__(self,loadPath):
+		return
+
+
+	@staticmethod
+	def pre_process_data(QAIs,labels_NER):
+		return
+
+	def fit(self,X,Y):
+		return
+
+	def transform(self):
+		return
+
+	def predict(self,X):
+		return 
+
+	def save_model(self,savePath):
